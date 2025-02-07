56235bded4b9d5454eca296b286312164474b5f2
mlipyansky <146876668+mlipyansky@users.noreply.github.com>
2025-02-06 19:16:03 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -10,15 +10,15 @@ int main() {
 
     // Create some dishes
     Dish dish1("Spaghetti", {"Pasta", "Tomato Sauce", "Basil"}, 20, 12.50, Dish::CuisineType::ITALIAN);
-    Dish dish2("Beef Stew", {"Beef", "Potatoes", "Carrots", "Onions", "Garlic"}, 90, 20.99, Dish::CuisineType::AMERICAN);  // Elaborate
+    Dish dish2("Beef Stew", {"Beef", "Potatoes", "Carrots", "Onions", "Garlic"}, 90, 20.99, Dish::CuisineType::AMERICAN); 
     Dish dish3("Tacos", {"Tortilla", "Beef", "Lettuce"}, 15, 9.99, Dish::CuisineType::MEXICAN);
-    Dish dish4("Pizza", {"Dough", "Tomato Sauce", "Cheese"}, 30, 14.99, Dish::CuisineType::ITALIAN);  // Elaborate
+    Dish dish4("Pizza", {"Dough", "Tomato Sauce", "Cheese"}, 30, 14.99, Dish::CuisineType::ITALIAN);  
 
     // Add dishes to the kitchen
     kitchen.newOrder(dish1);
-    kitchen.newOrder(dish2);  // Elaborate
+    kitchen.newOrder(dish2);  
     kitchen.newOrder(dish3);
-    kitchen.newOrder(dish4);  // Elaborate
+    kitchen.newOrder(dish4);  
 
     // Call kitchenReport to output the current state of the kitchen
     kitchen.kitchenReport();
